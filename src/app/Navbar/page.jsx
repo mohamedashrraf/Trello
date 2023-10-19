d55c80fa7158c0b9d76d3950ab386f6c3d03671b
Your Nameomar <omargamal.2tecno@gmail.com>
2023-10-19 04:23:56 -0800

    footer and nav

--- src/app/Navbar/page.jsx
+++ src/app/Navbar/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container">
     <Link className="navbar-brand" href="/">Trello</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,20 +18,8 @@ export default function Navbar() {
           <Link className="nav-link"  href='./login'>Login</Link>
             </li>
             <li className="nav-item">
-          <Link className="nav-link"  href='./signup'>Signup</Link>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      
+          <Link className="nav-link"  href='./signup'>Sign Up</Link>
+        </li>  
       </ul>
       
     </div>
